61647965ab168691483e3954428a315696e3cd17
ashraful2871 <ashrafulislam2758@gmail.com>
2025-10-05 15:34:53 +0600

    create blog and redirect successfglly

--- src/action/action.tsx
+++ src/action/action.tsx
@@ -1,35 +1,35 @@
 "use server";
 import { revalidateTag } from "next/cache";
 import { redirect } from "next/navigation";
+import { getServerSession } from "next-auth";
+import { authOptions } from "@/components/halper/authOption";
 
 export const create = async (data: FormData) => {
+  const session = await getServerSession(authOptions);
+
+  if (!session?.user?.id) throw new Error("User not authenticated");
+
   const blogInfo = Object.fromEntries(data.entries());
   const modifiedBlog = {
     title: blogInfo.title,
     content: blogInfo.content,
+    userId: parseInt(session.user.id),
   };
-
-  // Validate the blog information
-  if (!modifiedBlog.title || !modifiedBlog.content) {
-    throw new Error("Title and content are required");
-  }
 
   const res = await fetch(
     `${process.env.NEXT_PUBLIC_BASE_URL}/blog/create-blog`,
     {
       method: "POST",
-      headers: {
-        "content-type": "application/json",
-      },
+      headers: { "Content-Type": "application/json" },
       body: JSON.stringify(modifiedBlog),
+      credentials: "include",
     }
   );
 
   const result = await res.json();
 
-  if (result?.id) {
-    revalidateTag("BLOGS");
-    redirect("/blogs");
-  }
-  return result;
+  if (!result?.id) throw new Error("Blog creation failed");
+
+  revalidateTag("BLOGS");
+  redirect("/blogs");
 };
