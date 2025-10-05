9dd86336240ebc02489e40b8415cc8b8b01bb310
ashraful2871 <ashrafulislam2758@gmail.com>
2025-10-05 06:08:32 +0600

    add emample file

---
+++ example.tsx
@@ -0,0 +1,4 @@
+// # NEXT_PUBLIC_BASE_URL=http://localhost:5000/api/v1
+// # GOOGLE_CLIENT_SECRET=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
+// # GOOGLE_CLIENT_ID=272396830966-te19f0i2sfvbc16khelnjdj486p8obnn.apps.googleusercontent.com
+// # NEXTAUTH_SECRET=hJfLZjq3h1RcmvqXb3q2fK5vwvK0D1RgfXf2d7zVh9s
